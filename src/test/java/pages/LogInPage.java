89a7a7beb5284c91db4e7cfca4ab47ca5e0299f4
imark0007 <arkatalukder99@gmail.com>
2024-05-13 20:41:04 +0600

    WebAutomationB_23

--- src/test/java/pages/LogInPage.java
+++ src/test/java/pages/LogInPage.java
@@ -7,6 +7,7 @@ public class LogInPage extends BasePage{
 
         public String email = "jiwom54235@rehezb.com";
         public String wrongEmail = "jiwom54235@re.com";
+//      public String invalidEmail = "invalid_email";
         public String password = "112233445566";
         public String wrongPassword = "112233445566778899";
 //      public String phoneNumber = "+19174195208";
@@ -14,5 +15,6 @@ public class LogInPage extends BasePage{
         public By passwordInputBox = By.xpath("//input[@placeholder='Password']");
         public By logInButton = By.xpath("//button[normalize-space()='Login']");
         public By errorMessage = By.xpath("//p[normalize-space()='Your email or password is incorrect!']");
+
 
 }
